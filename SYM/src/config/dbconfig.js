6934d324f3216ad0571bffb80670d0a82133c0e1
menakafernando <project.menaka@gmail.com>
2016-11-25 14:32:15 +0530

    Fixed popping stash

--- SYM/src/config/dbconfig.js
+++ SYM/src/config/dbconfig.js
@@ -14,7 +14,7 @@ var connection = connector(mysql,{
 
   host: 'localhost',//host
   user: 'root',//username
-  password : '',//password
+  password : '123',//password
   port : 3306, //port mysql
   database:'dbsym'//db name
 },'pool'); //or single

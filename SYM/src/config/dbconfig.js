28e458b7cb6163ef7674ed992ff3cb63b53943da
Lightway <lightway29@gmail.com>
2016-11-28 16:21:09 +0530

    db configuration update

--- SYM/src/config/dbconfig.js
+++ SYM/src/config/dbconfig.js
@@ -4,11 +4,17 @@ var connector  = require('express-myconnection');
 
 var connection = connector(mysql,{
 
-    host: 'localhost',//host
+    // host: 'localhost',//host
+    // user: 'root',//username
+    // password : '123',//password
+    // port : 3306, //port mysql
+    // database:'dbsym'//db name
+
+    host: 'blog.cqq8bmen57bt.us-east-1.rds.amazonaws.com',//host
     user: 'root',//username
-    password : '123',//password
+    password : 'adminsaveyourmonkey',//password
     port : 3306, //port mysql
-    database:'dbsym'//db name
+    database:'blog'//db name
 
 },'pool'); //or single
 
